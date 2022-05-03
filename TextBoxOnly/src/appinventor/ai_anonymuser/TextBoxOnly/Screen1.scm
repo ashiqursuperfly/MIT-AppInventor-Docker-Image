eb597deacf9394840b905f75cfe543d4373ef789
ashiqursuperfly <ashiqur.buet16@gmail.com>
2022-05-03 16:23:19 +0600

    refactor

--- TextBoxOnly/src/appinventor/ai_anonymuser/TextBoxOnly/Screen1.scm
+++
@@ -1,5 +0,0 @@
-#|
-$JSON
-{"authURL":["localhost"],"YaVersion":"213","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"27","AppName":"TextBoxOnly","Title":"Screen1","Uuid":"0","$Components":[{"$Name":"TextBox1","$Type":"TextBox","$Version":"6","Hint":"Hint for TextBox1","Uuid":"-256063562"}]
-}}
-|#
